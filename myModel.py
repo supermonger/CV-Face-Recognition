e8a8f424b0b0d224606f7ba6e63efaf759a37526
Danny-Chien <kelly43122571@gmail.com>
2022-05-21 11:47:52 +0800

    add mobilenetv2 and movilenetv3

--- myModel.py
+++ myModel.py
@@ -1,17 +1,54 @@
 import torch.nn as nn
-from torchvision.models import resnet34
+from torchvision.models import resnet34, mobilenet_v2, mobilenet_v3_small
 
-class myModel(nn.Module):
-    def __init__(self, num_classes = 2):
+
+class myMobileNet_V3(nn.Module):
+    def __init__(self, num_classes=2):
         super().__init__()
-        self.resnet = resnet34(pretrained=True)
-        self.fc1 = nn.Sequential(nn.Linear(self.resnet.fc.out_features, num_classes))
+        self.mobilenet = mobilenet_v3_small(pretrained=True)
+        self.fc1 = nn.Sequential(
+            nn.Linear(self.mobilenet.classifier[3].out_features, 512),
+            nn.ReLU(),
+        )
+        self.fc2 = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.ReLU(),
+        )
+        self.fc3 = nn.Linear(256, num_classes)
 
     def forward(self, x):
-        out = self.resnet(x)
+        out = self.mobilenet(x)
         out = self.fc1(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+
         return out
 
+
+class myMobileNet_V2(nn.Module):
+    def __init__(self, num_classes=2):
+        super().__init__()
+        self.mobilenet = mobilenet_v2(pretrained=True)
+        self.fc1 = nn.Sequential(
+            nn.Linear(self.mobilenet.classifier[1].out_features, 512),
+            nn.ReLU(),
+        )
+        self.fc2 = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.ReLU(),
+        )
+        self.fc3 = nn.Linear(256, num_classes)
+
+    def forward(self, x):
+        out = self.mobilenet(x)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+
+        return out
+
+
 def get_model(num_classes):
-    model = myModel(num_classes)
+    # model = myModel(num_classes)
+    model = myMobileNet_V3(num_classes)
     return model
