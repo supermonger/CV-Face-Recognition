30e45150e6fc98eccd2f3e12dc9ea5f765f32324
Po-Wei Huang <81707116+supermonger@users.noreply.github.com>
2022-05-25 23:04:37 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,142 @@
+from tkinter import Image
+import torch
+from myDataset import get_dataloader
+from torchvision import transforms
+from myModel import get_model
+import torch.nn as nn
+import torch.optim as optim
+from tqdm import tqdm
+import numpy as np
+import os
+
+
+def cal_NMEloss(ldmk, pts68_gt):
+    ldmk = ldmk.cpu().detach().numpy()
+    pts68_gt = pts68_gt.cpu().detach().numpy()
+    dis = ldmk - pts68_gt
+    dis = np.sqrt(np.sum(np.power(dis, 2), 2))
+    dis = np.mean(dis, axis=1)
+    x = dis / 384
+    return np.sum(x, axis=0)
+
+
+def preprocessing(batch=64):
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+    train_transform = transforms.Compose(
+        [
+            transforms.Resize((224, 224)),
+            # transforms.RandomRotation(30),
+            # transforms.RandomPerspective(distortion_scale=0.3, p=0.3),
+            # transforms.Grayscale() paper have used, need to change model channel
+            transforms.ToTensor(),
+            transforms.Normalize(mean, std),
+        ]
+    )
+    valid_transform = transforms.Compose(
+        [
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean, std),
+        ]
+    )
+    train_loader, valid_loader = get_dataloader(train_transform, valid_transform, batch)
+
+    return train_loader, valid_loader
+
+
+def train(train_loader, valid_loader):
+
+    model = get_model(num_classes=68 * 2)
+    print(model)
+    loss_fn = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=1e-5, weight_decay=1e-6)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, 10, 0.2)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # device = torch.device("cpu")
+    num_epoch = 50
+    best_NME = 10**3
+    model.to(device)
+    
+    for epoch in range(num_epoch):
+        print(f"epoch{epoch}")
+        train_loss, valid_loss, train_NME, valid_NME = 0.0, 0.0, 0.0, 0.0
+        model.train()
+
+        for img, landmark in tqdm(train_loader):
+
+            img = img.to(device)
+            landmark = landmark.to(device)
+            out = model(img)
+            out = out.reshape(out.shape[0], 68, 2)
+
+            loss = loss_fn(out, landmark)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item()
+            train_NME += cal_NMEloss(out, landmark)
+
+        scheduler.step()
+        train_loss = train_loss / len(train_loader.dataset)
+        train_NME = train_NME / len(train_loader.dataset)
+
+        model.eval()
+        with torch.no_grad():
+
+            for img, landmark in tqdm(valid_loader):
+                img = img.to(device)
+                landmark = landmark.to(device)
+                out = model(img)
+                out = out.reshape(out.shape[0], 68, 2)
+
+                loss = loss_fn(out, landmark)
+
+                valid_loss += loss.item()
+                valid_NME += cal_NMEloss(out, landmark)
+
+        valid_loss = valid_loss / len(valid_loader.dataset)
+        valid_NME =  valid_NME / len(valid_loader.dataset)
+
+        print(f"train loss is {train_loss} and NME is {train_NME}")
+        print(f"validation loss is {valid_loss} and NME is {valid_NME}")
+    
+        if valid_NME < best_NME:
+            best_NME = valid_NME
+            print("Save the best model !")
+            torch.save(model.state_dict(), "best.pt")
+
+
+def prediction(result, batch_idx, batch):
+    result_path = os.path.join(os.path.join(os.path.curdir, "data"), "prediction")
+    result = result.cpu().detach().numpy()
+    if os.path.isdir(result_path) is False:
+        os.mkdir(result_path)
+
+    for i in range(result.shape[0]):
+        number = (batch_idx * batch) + i
+        h = number // 100
+        ten = (number - h * 100) // 10
+        las = number - h * 100 - ten * 10
+        fname = f"pred{h}{ten}{las}.txt"
+        with open(os.path.join(result_path, fname), "w") as f:
+            for j in range(result.shape[1]):
+                f.write(f"{result[i,j,0]} {result[i,j,1]}\n")
+
+
+def test(checkpoint, valid_loader, batch):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = get_model(num_classes=68 * 2)
+    model.load_state_dict(torch.load(checkpoint))
+    model.to(device)
+
+    model.eval()
+    with torch.no_grad():
+        for batch_idx, (img, landmark) in enumerate(tqdm(valid_loader)):
+            img = img.to(device)
+            landmark = landmark.to(device)
+            out = model(img)
+            out = out.reshape(out.shape[0], 68, 2)
+            prediction(out, batch_idx, batch)
